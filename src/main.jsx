dee34632c8f212f24c76eabf748eab6eb2f3b8f3
Alvaro Martin <almarlop98@gmail.com>
2025-09-16 18:40:18 +0200

    feat: add Vercel Analytics and Speed Insights components to main application

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { Provider } from "@/components/provider.jsx"
 import { BrowserRouter  } from "react-router-dom"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
@@ -9,6 +11,8 @@ createRoot(document.getElementById("root")).render(
     <Provider>
       <BrowserRouter>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </BrowserRouter>
     </Provider>
   </StrictMode>,
